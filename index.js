c21928cd0d3d8609de12506f6ab5eaa6e9625581
devmork <enoymarkden@gmail.com>
2025-06-16 21:29:06 +0800

    Increment humanScore and computerScore based on the round winner

--- index.js
+++ index.js
@@ -20,16 +20,22 @@ let computerScore = 0;
 function playRound(humanChoice, computerChoice) {
   if (humanChoice == "Rock" && computerChoice == "Scissors") {
     console.log("You win! Rock beats Scissors");
+    humanScore++;
   } else if (humanChoice == "Rock" && computerChoice == "Paper") {
     console.log("You lose! Rock loses to Paper");
+    computerScore++;
   } else if (humanChoice == "Paper" && computerChoice == "Rock") {
     console.log("You win! Paper beats Rock");
+    humanScore++;
   } else if (humanChoice == "Paper" && computerChoice == "Scissors") {
     console.log("You lose! Paper loses to Scissors");
+    computerScore++;
   } else if (humanChoice == "Scissors" && computerChoice == "Paper") {
     console.log("You win! Scissors beats Paper");
+    humanScore++;
   } else if (humanChoice == "Scissors" && computerChoice == "Rock") {
     console.log("You lose! Scissors loses to Rock");
+    computerScore++;
   } else {
     console.log("Draw");
   }
